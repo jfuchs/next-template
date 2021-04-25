aabca5690d3cdceab8a361bfd251f97478796ccd
Jonathan Fuchs <jonathan.fuchs@gmail.com>
2021-04-24 21:34:41 -0700

    do easy method

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,7 @@ import { times } from "lodash"
 
 let state = newCounterState()
 
-times(5, () => {
+times(5, (): void => {
   const oldCount = state.count
   state = counterReducer(state, { type: "increment", delta: 2 })
   console.warn(`${oldCount} → ${state.count}`)
