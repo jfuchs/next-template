23c1d9c4dae161ad137bfb58a02dbe65b9c82746
Jonathan Fuchs <jonathan.fuchs@gmail.com>
2021-08-20 17:48:36 -0700

    blah sorry

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react"
 import { useAppState } from "@/src/hooks/useAppState"
 
 export const App = () => {
-  const [{ message, count }, dispatch] = useAppState()
+  const [{ count }, dispatch] = useAppState()
+  const [greeting, setGreeting] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if (typeof window === "undefined") return
+    if (typeof greeting !== "undefined") return
+    fetch("/api/greeting")
+      .then((response) => response.json())
+      .then(({ message }) => setGreeting(message))
+  })
 
   return (
     <>
-      <h1 className="text-6xl text-blue-800">{message}</h1>
+      <h1 className="text-6xl text-blue-800">{greeting || " "}</h1>
       <h1 className="text-6xl text-yellow-800">Count: {count}</h1>
       <button
         onClick={() => dispatch({ type: "increment" })}
